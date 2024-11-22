67ef93d26188948221dcca3e92e3dee538d44487
Sparrow8104 <anchalsingh8102004@gmail.com>
2024-11-22 09:59:59 +0530

    Added a login page with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Register from './components/auth/Register'
 import { Routes,Route } from 'react-router-dom'
+import Login from './components/auth/Login'
 function App() {
   return (
     <Routes>
        < Route path='/register' element={<Register/>}/>
+       <Route path='/login' element={<Login/>
+       }/>
     </Routes>
   )
 }
--- src/components/ui/backToLogin.jsx
+++ src/components/ui/backToLogin.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './backToLogin.css'
 import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 const BackToLogin=() => {
+    const navigate=useNavigate()
+    const navigateHandler= () =>{
+    navigate('/Login')
+    }
   return (
-    <div className="ui_back_to_login">
+    <div onClick={navigateHandler} className="ui_back_to_login">
        <FaArrowLeft/>
        <span>back to login</span>
     </div>
