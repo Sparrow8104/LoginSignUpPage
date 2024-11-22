7ce12c30ac78738b1064045cc9faeb6d83c87a20
Sparrow8104 <anchalsingh8102004@gmail.com>
2024-11-22 10:21:39 +0530

    Added a forgetPassword page with some styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Register from './components/auth/Register'
 import { Routes,Route } from 'react-router-dom'
 import Login from './components/auth/Login'
+import ForgetPassword from './components/auth/ForgetPassword'
 function App() {
   return (
     <Routes>
        < Route path='/register' element={<Register/>}/>
-       <Route path='/login' element={<Login/>
-       }/>
+       <Route path='/login' element={<Login/>}/>
+       <Route path='/forget/password' element={<ForgetPassword/>}/>
     </Routes>
   )
 }
---
+++ src/components/auth/ForgetPassword.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Input from '../ui/input'
+import Button from '../ui/Button'
+import BackToLogin from '../ui/backToLogin'
+import { MdEmail } from "react-icons/md";
+function ForgetPassword() {
+
+
+    const [email, setEmail] = useState('');
+    const emailChange = (event) => setEmail(event.target.value);
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log({ email});
+      };
+  return (
+  <div className='auth_main'>
+    <form onSubmit={submitHandler}>
+        <div className="auth_container">
+            <div className="auth_header">
+                < MdEmail />
+                <p className='auth_heading'>Forget your password</p>
+                <p className='auth_title'>Enter your email address and we will send a 6-digit otp</p>
+            </div>
+            <div className="auth_item">
+                <label >Email</label>
+                <Input onChange={emailChange} type="email" placeholder="enter your email"/>
+            </div>
+            <div className="auth_action">
+                <Button>Send OTP</Button>
+            </div>
+            <div>
+                <BackToLogin/>
+            </div>
+        </div>
+    </form>
+    </div>
+  )
+}
+
+export default ForgetPassword
